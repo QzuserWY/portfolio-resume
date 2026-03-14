1cbc2d09b33d80cd1b25caed3f1cd24ab447fbe4
Qzuser <qzuser@QzuserdeMac-mini.local>
2026-03-14 23:18:50 +0800

    修复构建并重新部署

--- src/vue/stack/DataManager.vue
+++ src/vue/stack/DataManager.vue
@@ -106,7 +106,7 @@ const _loadSectionJsonFiles = async () => {
 
 const _loadJson = async (path) => {
     try {
-        const response = await fetch(basePath + "/data/" + path)
+        const response = await fetch(basePath + path)
         return await response.json()
     }
     catch (e) {
